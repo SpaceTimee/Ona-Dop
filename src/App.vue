636202de1fa08124743708a046e9cd25a5c816eb
Space Time <55327009+SpaceTimee@users.noreply.github.com>
2024-03-13 22:14:10 +0800

    1.0.0 -> 1.0.1 第8次更新

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { SpeedInsights } from '@vercel/speed-insights/vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <template>
