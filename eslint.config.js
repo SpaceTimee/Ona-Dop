e4342d107c4062a7b2d16e97964b712a8afd2c11
Space Time <Zeus6_6@163.com>
2025-12-05 16:24:15 +0800

    1.0.0 -> 1.0.1 第21次更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import cypress from 'eslint-plugin-cypress/flat'
 
-export default [
+export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}']
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
+  vueTsConfigs.recommended,
   skipFormatting,
-  
-  cypress.configs.recommended,
-]
+
+  cypress.configs.recommended
+)
